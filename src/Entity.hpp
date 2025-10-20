ebd2b7aa4554a662d3723725c6bcf5fb4cb2c231
theflyoccultist <y.delahaije@gmail.com>
2025-10-20 11:40:17 +0200

    entity api

--- src/Entity.hpp
+++ src/Entity.hpp
@@ -2,11 +2,12 @@
 #include <raylib.h>
 #include <vector>
 
+enum class EntityTypes { ALLY, ENEMY, NPC };
+
 using std::vector;
 
-class Entity {
-  enum class EntityTypes { ALLY, ENEMY, NPC };
-
+class EntityManager {
+public:
   struct EntityState {
     EntityTypes type;
     Vector2 position;
@@ -21,26 +22,33 @@ private:
 public:
   void spawnEnemies() {
     enemies.push_back({(EntityTypes::ENEMY), {400, 200}, {0, 0}, 30, 120.0f});
+    enemies.push_back({(EntityTypes::ENEMY), {680, 300}, {0, 0}, 500, 90.0f});
   }
 
-  void updateEnemies(vector<EntityState> enemies) {
+  void updateEnemies() {
     for (auto &enemy : enemies) {
       enemy.position.x +=
           enemy.direction.x * enemy.entitySpeed * Game::deltaTime;
+      enemy.position.y +=
+          enemy.direction.y * enemy.entitySpeed * Game::deltaTime;
     }
   }
 
-  void drawEnemies(Vector2 enemyPosition, int type) {
-    DrawRectangle(700, 100, 100, 100, BLUE);
-    DrawRectangle(650, 200, 100, 100, BLUE);
+  void drawEnemies() {
+    for (auto &enemy : enemies) {
+      DrawRectangleV(enemy.position, {100, 100}, BLUE);
+    }
   }
 
   bool checkPlayerInteraction(Vector2 playerPos, const EntityState &entity,
                               float entitySize = 100.0f) {
 
     Rectangle playerRect = {playerPos.x, playerPos.y, 70, 70};
-    Rectangle entityRect = {entity.position.x, entity.position.y, 100, 100};
+    Rectangle entityRect = {entity.position.x, entity.position.y, entitySize,
+                            entitySize};
 
     return CheckCollisionRecs(playerRect, entityRect);
   }
+
+  const vector<EntityState> &getEnemies() const { return enemies; }
 };
