325510e8de3989210538a85eefb44fef09769261
theflyoccultist <y.delahaije@gmail.com>
2025-11-24 15:09:36 +0100

    coming soon: make projectile data scriptable

---
+++ scripts/weaponData.lua
@@ -0,0 +1,7 @@
+Weapons = {
+	{ type = "longrange", speed = 400, fireRate = 0.1, size = 8, lifetime = 3, damage = 60, spread = 4 },
+	{ type = "slowcannon", speed = 800, fireRate = 0.08, size = 3, lifetime = 6, damage = 100, spread = 1 },
+	{ type = "straight", speed = 1600, fireRate = 0.21, size = 5, lifetime = 8, damage = 22, spread = 3 },
+	{ type = "uzi", speed = 560, fireRate = 0.3, size = 10, lifetime = 1, damage = 10, spread = 8 },
+	{ type = "rocket", speed = 444, fireRate = 0.15, size = 20, lifetime = 3, damage = 40, spread = 4 },
+}
