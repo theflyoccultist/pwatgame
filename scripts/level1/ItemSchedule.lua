11c48205d03ad21a39428aa64b41ed20c9f9900f
Rin Delahaije <y.delahaije@gmail.com>
2025-12-13 01:26:56 +0100

    remove useless file

--- scripts/level1/ItemSchedule.lua
+++ scripts/level1/ItemSchedule.lua
@@ -8,4 +8,8 @@ DrinkSpawns = {
 
 WeaponSpawns = {
 	{ x = 230, y = 331, delay = 0.0, type = "rocket", ammoBonus = 10 },
+	{ x = 410, y = 531, delay = 5.9, type = "hellfire", ammoBonus = 70 },
+	{ x = 300, y = 131, delay = 10.9, type = "straight", ammoBonus = 20 },
+	{ x = 490, y = 631, delay = 13.9, type = "uzi", ammoBonus = 50 },
+	{ x = 60, y = 80, delay = 17.9, type = "grenade", ammoBonus = 6 },
 }
