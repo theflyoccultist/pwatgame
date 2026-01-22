9bcdbb58e1c1f2b03728e35d708fe54d32909809
theflyoccultist <y.delahaije@gmail.com>
2026-01-22 18:04:31 +0100

    done for now, rust miniboss for later

--- Linux_Build.sh
+++ Linux_Build.sh
@@ -5,4 +5,7 @@ mkdir -p build
 cmake -B build -DCMAKE_TOOLCHAIN_FILE=toolchains/linux.cmake
 cmake --build build
 
-git ls-files '*.cpp' '*.hpp' | xargs wc -l
+printf "C++ LOC :\n"
+git ls-files '*.cpp' '*.hpp' | xargs wc -l | tail -n 1
+printf "lua LOC :\n"
+git ls-files '*.lua' | xargs wc -l | tail -n 1
